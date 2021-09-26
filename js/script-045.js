e7bf338e5f3b44c396a5cba2412d94e17d10a68f
smdmori <mortezasamadi1371@gmail.com>
2021-09-27 00:32:20 +0330

    // IIFE and Safe code

--- js/script-045.js
+++ js/script-045.js
@@ -1,14 +1,18 @@
-var greeting = 'Hello'
-
 // IIFE and Safe code
+
+// global variable
+var greeting = 'Hello';
+
+// IIFE
 (function(global, name) {
     var greeting = 'Salam';
 
     // mutate global variable intentially and safe
-    global.greeting = 'Hallo'
-    console.log(greeting + ' ' + name);
-}(window, 'World'))
+    global.greeting = 'Hallo';
 
+    console.log(greeting + ' ' + name);
+
+}(window, 'World'));
 
 // Global
 console.log(greeting);
