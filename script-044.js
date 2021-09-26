cec13336e904465b35370c02d2d87cc3e90817c6
smdmori <mortezasamadi1371@gmail.com>
2021-09-27 00:04:33 +0330

    IIFE

---
+++ script-044.js
@@ -0,0 +1,27 @@
+var myName = 'Mori'
+// function statement
+function greet(name) {
+    console.log('Hi '+ name);
+}
+
+greet(myName);
+
+// function expression
+var greetFunc = function(name) {
+    console.log('inside func : Hi ' + name);
+}
+
+greetFunc(myName);
+
+
+
+// Immidiately Invoked Function Expression (IIFE)
+var greeting = function(name) {
+    console.log('Inside IIFE : Hi ' + name);
+}(myName);
+
+
+// IIFE
+(function(name) {
+    console.log('Hello : ' + name);
+})(myName)
