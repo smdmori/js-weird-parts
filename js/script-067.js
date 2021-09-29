ba49185ab171d86789a7bef14103b47c5fbc9e1a
smdmori <mortezasamadi1371@gmail.com>
2021-09-29 16:58:49 +0330

    using strict mode

---
+++ js/script-067.js
@@ -0,0 +1,19 @@
+// strict mode
+function useStrict() {
+    "use strict";
+    // TODO: using strict mode locally in this function
+    // If I add this string to the top of the file, it will behave globally
+
+    var person2;
+    persom2 = {}
+    console.log(persom2);
+}
+
+
+// not using strict mode
+var person;
+
+persom = {};
+console.log(persom);
+
+useStrict();
