aad13d910776f1ff750bd795ed84696ef112131a
smdmori <mortezasamadi1371@gmail.com>
2021-09-30 12:48:26 +0330

    Properties and Chainable Methods (076.js)

--- js/Greetr.js
+++ js/Greetr.js
@@ -4,10 +4,86 @@
         return new Greetr.init(firstName, lastName, language)
     }
 
-    Greetr.prototype = {};
+
+    var supportedLanguages = ['en', 'es'];
+
+    var greetings = {
+        en: 'Hello ',
+        es: 'Hola'
+    };
+
+    var formalGreetings = {
+        en: 'Greetings',
+        es: 'Saludos'
+    };
+
+    var logMessages = {
+        en: 'Logged in',
+        es: 'inició sesión'
+    }
+
+    Greetr.prototype = {
+        fullName: function () {
+            return this.firstName + ' ' + this.lastName;
+        },
+
+        validate: function () {
+            if (supportedLanguages.indexOf(this.language) === -1) {
+                throw 'Invalid Language';
+            }
+        },
+
+        greeting: function () {
+            return greetings[this.language] + ' ' + this.firstName + '!';
+        },
+
+        formalGreeting: function () {
+            return formalGreetings[this.language] + ', ' + this.fullName() + '!';
+        },
+
+        greet: function (formal) {
+
+            var msg;
+
+            // if undefined of null it will be coerced to false
+            if (formal) {
+                msg = this.formalGreeting();
+            }
+            else {
+                msg = this.greeting();
+            }
+
+            if (console) {
+                console.log(msg);
+            }
+
+            // 'this' refers to the calling object at execution time
+            // makes the method chainable
+            return this;
+        },
+
+        log: function () {
+            if (console) {
+                // FIXME:
+                console.log(logMessages[this.language + ': ' + this.fullName]);
+            }
+
+            return this;
+        },
+
+        setLang: function (lang) {
+
+            this.language = lang;
+            this.validate();
+
+            return this;
+        }
+
+
+    };
 
     Greetr.init = function (firstName, lastName, language) {
-        
+
         var self = this;
         self.firstName = firstName || '';
         self.lastName = lastName || '';
--- js/script-074.js
+++ js/script-074.js
@@ -1,3 +1,6 @@
+// Properties and Chainable Methods
 var g = G$('Mori', 'Sami');
 
 console.log(g);
+
+g.greet().setLang('es').greet(true).setLang('en').greet(true);
